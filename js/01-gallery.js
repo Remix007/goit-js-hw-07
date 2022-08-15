1158bd31faf8deb08e22322cd55c9c2a323e6969
Remix007 <forvard16back@gmail.com>
2022-08-15 18:48:15 +0300

    maked work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,67 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+/*
+Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+*/
+
+const listGallary = document.querySelector(".gallery");
+
+const makeGallaryCards = ({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
+
+const galleryArray = galleryItems.map((el) => {
+    return makeGallaryCards(el);
+});
+
+listGallary.insertAdjacentHTML("afterbegin", galleryArray.join(""));
+
+const onlistGallaryClick = (event) => {
+    event.preventDefault();
+
+    if (event.target.tagName !== "IMG") {
+        return;
+    }
+
+    let imgSRC = event.target.src;
+    const linkHREF = event.target.parentNode.href;
+
+    imgSRC = linkHREF;
+
+    const instance = basicLightbox.create(
+        `
+    <img src="${linkHREF}" class="gallery__image"
+>
+`,
+        {
+            onShow: (instance) => {
+                window.addEventListener("keydown", onEscPressKey);
+            },
+            onClose: (instance) => {
+                window.removeEventListener("keydown", onEscPressKey);
+            },
+        }
+    );
+
+    instance.show();
+
+    function onEscPressKey(event) {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    }
+};
+
+listGallary.addEventListener("click", onlistGallaryClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. 
+// Використовуй готовий код з першого завдання.
+const listGallary = document.querySelector(".gallery");
+
+const makeGallaryCards = ({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+  };
+
+  const galleryArray = galleryItems.map((el) => {
+    return makeGallaryCards(el);
+});
+
+    listGallary.insertAdjacentHTML("afterbegin", galleryArray.join(""));
+
+const onlistGallaryClick = (event) => {
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+
+  let imgSRC = event.target.src;
+  const linkHREF = event.target.parentNode.href;
+ 
+  imgSRC = linkHREF;
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+};
+
+
+listGallary.addEventListener("click", onlistGallaryClick);
