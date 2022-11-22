4ab3984747fcd365fff4c1240a22409762091bfb
Remix007 <forvard16back@gmail.com>
2022-11-22 02:34:39 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,58 +10,58 @@ import { galleryItems } from "./gallery-items.js";
 const listGallary = document.querySelector(".gallery");
 
 const makeGallaryCards = ({ preview, original, description }) => {
-    return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+  return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
     <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
     />
-  </a>
+</a>
 </div>`;
 };
 
 const galleryArray = galleryItems.map((el) => {
-    return makeGallaryCards(el);
+  return makeGallaryCards(el);
 });
 
 listGallary.insertAdjacentHTML("afterbegin", galleryArray.join(""));
 
 const onlistGallaryClick = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (event.target.tagName !== "IMG") {
-        return;
-    }
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
 
-    let imgSRC = event.target.src;
-    const linkHREF = event.target.parentNode.href;
+  let imgSRC = event.target.src;
+  const linkHREF = event.target.parentNode.href;
 
-    imgSRC = linkHREF;
+  imgSRC = linkHREF;
 
-    const instance = basicLightbox.create(
-        `
+  const instance = basicLightbox.create(
+    `
     <img src="${linkHREF}" class="gallery__image"
 >
 `,
-        {
-            onShow: (instance) => {
-                window.addEventListener("keydown", onEscPressKey);
-            },
-            onClose: (instance) => {
-                window.removeEventListener("keydown", onEscPressKey);
-            },
-        }
-    );
-
-    instance.show();
-
-    function onEscPressKey(event) {
-        if (event.code === "Escape") {
-            instance.close();
-        }
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscPressKey);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscPressKey);
+      },
     }
+  );
+
+  instance.show();
+
+  function onEscPressKey(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
 };
 
 listGallary.addEventListener("click", onlistGallaryClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+console.clear();
 
-
-// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. 
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 // Використовуй готовий код з першого завдання.
 const listGallary = document.querySelector(".gallery");
 
 const makeGallaryCards = ({ preview, original, description }) => {
-    return `<a class="gallery__item" href="${original}">
+  return `<a class="gallery__item" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
   </a>`;
-  };
+};
 
-  const galleryArray = galleryItems.map((el) => {
-    return makeGallaryCards(el);
+const galleryArray = galleryItems.map((el) => {
+  return makeGallaryCards(el);
 });
 
-    listGallary.insertAdjacentHTML("afterbegin", galleryArray.join(""));
+listGallary.insertAdjacentHTML("afterbegin", galleryArray.join(""));
 
 const onlistGallaryClick = (event) => {
   event.preventDefault();
@@ -26,16 +25,10 @@ const onlistGallaryClick = (event) => {
     return;
   }
 
-  let imgSRC = event.target.src;
-  const linkHREF = event.target.parentNode.href;
- 
-  imgSRC = linkHREF;
-
-  var lightbox = new SimpleLightbox(".gallery a", {
+  const lightbox = new SimpleLightbox(".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
   });
 };
-
 
 listGallary.addEventListener("click", onlistGallaryClick);
